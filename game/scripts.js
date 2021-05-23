1983134e86b3b87df87239b8b255baad25299211
Felipe Anderson <59267574+felipeandersonr@users.noreply.github.com>
2021-05-22 23:03:07 -0300

    added style on the page

--- game/scripts.js
+++ game/scripts.js
@@ -1,10 +1,9 @@
-
 const player = document.querySelector('#player')
 
 player.style.x += 1
 
 
 document.body.addEventListener("keydown", (e) => {
-    player.style.top = player.style.top == '55%'? '35%':'55%'
+    player.style.top = player.style.top == '70%'? '45%':'70%'
    
   });
