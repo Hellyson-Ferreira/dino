4a7fc9f60040ab6d4e1230364967251c4f585a6d
Hellyson Ferreira <37805353+Hellyson-Ferreira@users.noreply.github.com>
2021-05-24 14:24:50 -0300

    Coligion

--- game/scripts.js
+++ game/scripts.js
@@ -1,10 +1,20 @@
 const player = document.querySelector('#player')
+const cactus = document.querySelector('#cactus')
 
 document.body.addEventListener("keydown", (e) => {
   if (e.keyCode == 32 || e.keyCode == 38){
-    if(player.classList){
-      
-    }
     player.classList.toggle('jump')
-    setTimeout(() => player.classList.toggle('jump'),400)
+    setTimeout(() => player.classList.toggle('jump'),800)
   }})
+
+
+  let colisao = setInterval(function(){
+    let dinoTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+    if(cactusLeft <50 && cactusLeft> 0 && dinoTop >= 140){
+      console.log('Perdeu')
+      alert('sdkdk')
+    }
+    // console.log(cactusLeft)
+  },10)
