b6c272da99148e0bd45058ab5e5ba0222924feeb
Hellyson Ferreira <37805353+Hellyson-Ferreira@users.noreply.github.com>
2021-05-24 12:58:01 -0300

    Added cacto

--- game/scripts.js
+++ game/scripts.js
@@ -2,6 +2,9 @@ const player = document.querySelector('#player')
 
 document.body.addEventListener("keydown", (e) => {
   if (e.keyCode == 32 || e.keyCode == 38){
+    if(player.classList){
+      
+    }
     player.classList.toggle('jump')
     setTimeout(() => player.classList.toggle('jump'),400)
   }})
